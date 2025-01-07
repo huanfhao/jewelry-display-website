dee989e50f2fba046ece4274e2a03b42cc195c98
huanfhao <2377104342@qq.com>
2025-01-07 08:54:41 +0800

    fix: simplify next.js config

--- next.config.js
+++ next.config.js
@@ -1,19 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
   images: {
     domains: ['res.cloudinary.com'],
-  },
-  experimental: {
-    serverActions: {
-      allowedOrigins: ['localhost:3000', 'jewelry-display-website.vercel.app']
-    },
-    optimizePackageImports: ['@mui/icons-material', '@mui/material'],
-    turbotrace: {
-      logLevel: 'error',
-      logDetail: false,
-      memoryLimit: 4096
-    }
   },
   typescript: {
     ignoreBuildErrors: true
@@ -21,62 +9,13 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true
   },
-  swcMinify: true,
-  staticPageGenerationTimeout: 120,
-  compiler: {
-    removeConsole: true
-  },
-  webpack: (config, { isServer }) => {
-    config.cache = true;
-
+  webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
       '@': require('path').resolve(__dirname, './src')
     };
-
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        crypto: false,
-        path: false,
-        os: false
-      };
-    }
-
-    config.optimization = {
-      ...config.optimization,
-      minimize: true,
-      splitChunks: {
-        chunks: 'all',
-        minSize: 20000,
-        maxSize: 244000,
-        cacheGroups: {
-          default: false,
-          vendors: false,
-          commons: {
-            name: 'commons',
-            chunks: 'all',
-            minChunks: 2,
-            priority: 20
-          },
-          vendor: {
-            name: 'vendor',
-            chunks: 'all',
-            test: /[\\/]node_modules[\\/]/,
-            priority: 10
-          }
-        }
-      }
-    };
-
     return config;
-  },
-  poweredByHeader: false,
-  generateBuildId: () => 'build',
-  productionBrowserSourceMaps: false,
-  reactStrictMode: false,
-  compress: true
+  }
 }
 
 module.exports = nextConfig 
